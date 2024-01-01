aa333ab5815d780aad0ba2651aefc9920ccd90f8
Mehran Khorasani <77451572+M1994Kh@users.noreply.github.com>
2024-01-01 18:02:43 +0330

    Update script.js
    final check

--- script.js
+++ script.js
@@ -17,33 +17,42 @@ function updateTextInput1(val) {
     document.getElementById('range').value = val;
     document.getElementById('num1').value = val;
 }
-document.getElementById('num1').value = val;
+
+
 let hrs=min=sec=ms=0,startTimer;
-hrs = Math.floor(val/60);
-// min = val%60;
-min = 5;
 
 btnStart.addEventListener('click',()=>{
-
-  btnStart.classList.add('start-active');
-  btnStop.classList.remove('stop-active');
-
-  startTimer=setInterval(()=>{
-    ms--;//ms=ms-1;
-    if(ms==0){
-      sec--;
-      ms=100;
+    btnStart.classList.add('start-active');
+    btnStop.classList.remove('stop-active');
+    if (hrs == 0 && min == 0 && sec == 0 && ms == 0)    {
+        val = document.getElementById('num1').value;
+        sec = 0;
+        ms = 0;
+        min = val%60;
+        hrs = Math.floor(val/60);
     }
-    if(sec==0){
-      min--;
-      sec=59;
-    }
-    if(min==0){
-      hrs--;
-      min=59;
-    }
+    // else {
+        
+    // }
+    
+    startTimer=setInterval(()=>{
+        if (hrs != 0 || min != 0 || sec != 0 || ms != 0)    {
+            btnStart.innerHTML = "Resume";
+            ms--;}//ms=ms-1;
+            if(ms==-1){
+            sec--;
+            ms=100;
+            }
+            if(sec==-1){
+            min--;
+            sec=59;
+            }
+            if(min==-1){
+            hrs--;
+            min=59;
+            }
     updateDisplay();
-  },10);
+    },10);
 });
 
 btnStop.addEventListener('click',()=>{
@@ -57,6 +66,7 @@ btnReset.addEventListener('click',()=>{
   hrs=min=sec=ms=0;
   clearInterval(startTimer);
   updateDisplay();
+  btnStart.innerHTML = "Start";
   btnStart.classList.remove('start-active');
   btnStop.classList.remove('stop-active');
 });
